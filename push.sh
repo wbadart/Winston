a7835936973e52644166089e6fb403d6277f471a
wbadart <wbadart@live.com>
2015-08-02 22:14:24 -0700

    Removed .sh's

--- push.sh
+++
@@ -1,4 +0,0 @@
-git add .
-git commit
-git push origin master
-git push . HEAD:origin
--- reset.sh
+++
@@ -1,7 +0,0 @@
-rm source/dictionary 
-rm source/sentences
-rm source/parameter_log
-touch source/dictionary
-touch source/sentences
-touch source/parameter_log
-ruby source/init.rb
